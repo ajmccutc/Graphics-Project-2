4f613c265b5756e78c0b757cdf249644716a6589
ajmccutc <mccutcheon.andrew.j@gmail.com>
2016-12-05 14:54:26 -0500

    Colour Change

--- Bacteria.js
+++ Bacteria.js
@@ -39,13 +39,13 @@ Bacteria.prototype.addTriangle = function(a, b, c) {
 Bacteria.prototype.draw = function(modelViewMatrix,projectionMatrix,program) {
 
     var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
-    var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+    var lightAmbient = vec4(1.0, 1.0, 0.0, 1.0);
     var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
     var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
     
     var materialAmbient = vec4(this.randomambient, 0.0, 1.0, 1.0);
     var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
-    var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
+    var materialSpecular = vec4(1.0, 0.0, 0.0, 1.0);
     var materialShininess = 100.0;
     
     
